9a0ecdc02b5ca5b80475cd40983110d05decdfb8
Sorokovsky <Sorokovskys@ukr.net>
2024-10-07 22:07:20 +0300

    task 2

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -2,8 +2,32 @@
 
 public static class Program
 {
+    private delegate int Delegate();
     public static void Main()
     {
-        
+        Console.WriteLine("Avarrage: {0}", Avarage([One, Two, Three]));
+    }
+
+    private static double Avarage(Delegate[] delegates)
+    {
+        double sum = 0;
+        foreach (Delegate delegation in delegates)
+        {
+            sum += delegation.Invoke();
+        }
+        return sum / delegates.Length;
+    }
+
+    private static int One()
+    {
+        return 1;
+    }
+    private static int Two()
+    {
+        return 2;
+    }
+    private static int Three()
+    {
+        return 3;
     }
 }
